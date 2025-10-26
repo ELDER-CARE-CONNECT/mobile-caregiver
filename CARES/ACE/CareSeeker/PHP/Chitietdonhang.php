75629bf4949268f05ee99c16ec0f2a07c64c72d7
Minh Thư <166976762+minhthu54@users.noreply.github.com>
2025-10-26 08:06:56 +0000

    Add orders table structure to Chitietdonhang.php

---
+++ CARES/ACE/CareSeeker/PHP/Chitietdonhang.php
@@ -0,0 +1,20 @@
+<div class="table-container">
+        <table class="orders-table">
+          <thead>
+            <tr>
+              <th>Mã đơn</th>
+              <th>Ngày đặt</th>
+              <th>Khách hàng</th>
+              <th>Người chăm sóc</th>
+              <th>Dịch vụ</th>
+              <th>Thời gian</th>
+              <th>Giá tiền</th>
+              <th>Trạng thái</th>
+              <th>Hành động</th>
+            </tr>
+          </thead>
+          <tbody id="ordersTableBody">
+            <!-- Data will be populated by JavaScript -->
+          </tbody>
+        </table>
+      </div>
