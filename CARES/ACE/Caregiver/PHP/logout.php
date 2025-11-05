c58fac62ba1a43ef7dc7c4c7573326fe66bee251
thanhdpv3214-crypto <thanhdpv3214@ut.edu.vn>
2025-11-05 07:36:11 +0700

    .

--- CARES/ACE/Caregiver/PHP/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();           // Bắt đầu session
-session_unset();           // Xóa toàn bộ biến session
-session_destroy();         // Hủy phiên đăng nhập
-
-// Quay lại trang đăng nhập caregiver
-header("Location: login.php");
-exit;
-?>
